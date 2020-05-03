fd5efd7c0f59c03378d41652f8c36e98fc2664a5
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-05-03 17:59:09 +0530

    Fix verify gentx script

--- scripts/akash_verify_gentx.sh
+++ scripts/akash_verify_gentx.sh
@@ -3,6 +3,7 @@
 set -e
 
 AKASH_HOME="/tmp/akash$(date +%s)"
+AKASHCTL_HOME="/tmp/akashctl$(date +%s)"
 RANDOM_KEY="randomvalidatorkeyxx"
 
 GENTX_FILE=$(ls centauri/gentxs -I gosuri.json | head -1)
@@ -18,22 +19,24 @@ else
     rm akash_linux.zip
     cd akash_0.6.1_linux_amd64
 
-    echo "12345678" | ./akashctl keys add $RANDOM_KEY --keyring-backend test
+    echo "12345678" | ./akashctl keys add $RANDOM_KEY --keyring-backend test --home $AKASHCTL_HOME
 
     ./akashd init --chain-id centauri testvalxyz --home $AKASH_HOME -o
 
     echo "..........Fetching genesis......."
     rm -rf $AKASH_HOME/config/genesis.json
-    curl -s https://raw.githubusercontent.com/ovrclk/net/master/centauri/genesis.json > $AKASH_HOME/config/genesis.json
+    curl -s https://raw.githubusercontent.com/ovrclk/net/master/centauri/genesis.json >$AKASH_HOME/config/genesis.json
 
     GENACC=$(cat ../centauri/gentxs/$GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p')
 
     echo $GENACC
 
-    echo "12345678" | ./akashd add-genesis-account $RANDOM_KEY 1000000000uakt --home $AKASH_HOME --keyring-backend test
+    echo "12345678" | ./akashd add-genesis-account $RANDOM_KEY 1000000000uakt --home $AKASH_HOME \
+        --keyring-backend test --home-client $AKASHCTL_HOME
     ./akashd add-genesis-account $GENACC 1000000000uakt --home $AKASH_HOME
 
-    echo "12345678" | ./akashd gentx --name $RANDOM_KEY  --home $AKASH_HOME --keyring-backend test
+    echo "12345678" | ./akashd gentx --name $RANDOM_KEY --amount 900000000uakt --home $AKASH_HOME \
+        --keyring-backend test --home-client $AKASHCTL_HOME
     cp ../centauri/gentxs/$GENTX_FILE $AKASH_HOME/config/gentx/
 
     cat $AKASH_HOME/config/genesis.json
@@ -43,7 +46,7 @@ else
     sed -i '/persistent_peers =/c\persistent_peers = ""' $AKASH_HOME/config/config.toml
 
     echo "..........Starting node......."
-    ./akashd start --home $AKASH_HOME &> /dev/null
+    ./akashd start --home $AKASH_HOME &
 
     sleep 5s
 
@@ -52,6 +55,6 @@ else
     ./akashctl status --chain-id centauri
 
     echo "...Cleaning the stuff..."
-    killall akashd > /dev/null 2>&1
-    rm -rf $AKASH_HOME > /dev/null 2>&1
+    killall akashd >/dev/null 2>&1
+    rm -rf $AKASH_HOME >/dev/null 2>&1
 fi
