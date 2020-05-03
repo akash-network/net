f2d4410474e9a4af8f73b91e9f07b6a8423bc285
anilCSE <anil@vitwit.com>
2020-05-03 15:31:35 +0530

    Fix path

--- scripts/akash_verify_gentx.sh
+++ scripts/akash_verify_gentx.sh
@@ -25,7 +25,7 @@ else
     echo "..........Fetching genesis......."
     curl -s https://raw.githubusercontent.com/ovrclk/net/master/centauri/genesis.json > $AKASH_HOME/config/genesis.json
 
-    cp centauri/gentxs/$GENTX_FILE $AKASH_HOME/config/gentx/
+    cp ./centauri/gentxs/$GENTX_FILE $AKASH_HOME/config/gentx/
 
     echo "12345678" | ./akashd add-genesis-account $RANDOM_KEY 9000000uakt --home $AKASH_HOME --keyring-backend test
     ./akashd add-genesis-account $(cat centauri/gentxs/$GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p') 9000000uakt --home $AKASH_HOME
